2b332c442bdb344e0038c8124a3f8c3e5000bed6
Nicolai <nalar94@gmail.com>
2017-04-10 12:21:13 +0200

    no message

---
+++ theWall/theWall/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="theWall.MvcApplication" Language="C#" %>
---
+++ theWall/theWall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(theWall.Startup))]
+namespace theWall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
